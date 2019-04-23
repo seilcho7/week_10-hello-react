613afcb30ed4ffc98f3e8a4db494ea1ebbabdcf3
Seil Cho <seilcho7@hotmail.com>
2019-04-23 13:58:29 -0400

    adds several clicky counters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Just like require() in your Node.js code
+// "ES6 style imports" (as opposed to calling require())
+import React from 'react';      // Pull in the React library itself
+                                // Put this in every .js file in your project
 
+import logo from './logo.svg';  // Import an image (React will do optimizations)
+import './App.css';             // Recommendation: no individual style files. Just use index.css
+
+import Greet from './Greet';
+import Counter from './Counter';
+// When you import 
+import ClickyCounter from './ClickyCounter';
+
+// "App" is a component
+// A component is a function that returns a React Element
+// A React Element is a description of a DOM element (or tree)
+// The most common way to specify a React Element is by using JSX
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ClickyCounter />
+        <ClickyCounter />
+        <ClickyCounter />
+        <Counter />
+        <Greet whom="Lovies ☠️" />
+        {/* This is a comment in JSX */}
+        
         <a
           className="App-link"
           href="https://reactjs.org"
